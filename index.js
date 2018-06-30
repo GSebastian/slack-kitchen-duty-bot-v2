b08ebb24a453ffeb9eef4fb3317e236bc17b92a2
Sebastian Ghetu <sebastian@ghetu.net>
2018-06-30 16:46:35 +0100

    More dev-ops work before I get into it

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 
 app.get('/', (req, res) => {
-  res.send('Testing 1 2 3');
+  res.send('Testing 1 2 3 4 5 6');
 });
 
 app.listen(3000, () => console.log('Server running'));
---
+++ start-docker.sh
@@ -0,0 +1,1 @@
+docker build -t slack-kitchen-duty-bot-v2 . && docker run -p 3000:3000 slack-kitchen-duty-bot-v2
